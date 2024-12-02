558fa792cc654fdd8d0647206b141f1f831eddfa
LucasSukino <lucasuemurasukino@hotmail.com>
2024-12-02 12:10:55 -0300

    terceiro commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CadastroUsuario from "./Telas/CadastroUsuario";
 import ExcluirAlterarUsuario from "./Telas/ExcluirAlterarUsuario";
 import BatePapoTela from "./Telas/BatePapoTela";
@@ -6,21 +6,25 @@ import Home from "./Telas/Home";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
-
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <HashRouter>
-          <Routes>
-            <Route path="/batepapogeral" element={<BatePapoTela />} />
-            <Route path="/usuario" element={<CadastroUsuario />} />
-            <Route path="/usuarioExcluirAlterar" element={<ExcluirAlterarUsuario />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </HashRouter>
+      <BrowserRouter>
+        <Routes>
+          {
+          }
+        
+          <Route path="/batepapogeral" element={<BatePapoTela />} />
+          <Route path="/usuario" element={<CadastroUsuario />} />
+          <Route path="/usuarioExcluirAlterar" element={<ExcluirAlterarUsuario />} />
+          <Route path="/" element={<Home />} />
+          {
+          }
+        </Routes>
+      </BrowserRouter>
       </Provider>
-      <ToastContainer />
+     <ToastContainer/>
     </div>
   );
 }
